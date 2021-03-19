0c4a3a1c0cd53a9519e965a04efd7554770cda85
91630 <jujjuriraji@gmail.com>
2021-03-19 18:41:29 +0530

    Third Commit

---
+++ String and Parsing/src/DurationDifference.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.Period;
+public class DurationDifference {
+	 static void durationDifference(LocalDate start_date,LocalDate end_date) 
+	    { 
+
+	        Period difference = Period .between(start_date, end_date); 
+
+	        System.out.print( "Difference "+ "between two dates is: "); 
+
+
+	        System.out.printf( "%d years, %d months,  and %d days ", difference.getYears(), difference.getMonths(), difference.getDays()); 
+	    } 
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 LocalDate start_date = LocalDate.of(2018, 01, 10); 
+
+
+	        LocalDate end_date  = java.time.LocalDate.now();
+
+	        durationDifference(start_date, end_date); 
+		
+
+	}
+
+}
---
+++ String and Parsing/src/NumberOfCharacters.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberOfCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter the string");
+		String str=sc.nextLine();
+		int lineLength=str.split("\\.").length;
+		int wordLength=str.split(" ").length+lineLength-1;
+		int characterCount=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)!=' '&&str.charAt(i)!='.')
+				characterCount++;
+
+		}
+		System.out.println(lineLength);
+		System.out.println(wordLength);
+		System.out.println(characterCount);
+
+
+	}
+
+}
